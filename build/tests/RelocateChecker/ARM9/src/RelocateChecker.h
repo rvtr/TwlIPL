765a4dfc96af41092645aa5e5c832d038ebe373f
yoshida_teruhisa <yoshida_teruhisa@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2007-12-06 07:50:32 +0000

    再配置チェッカープログラム追加（作成中……）
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@327 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/tests/RelocateChecker/ARM9/src/RelocateChecker.h
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlIPL
+  File:     RelocateChecker.h
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date:: 2007-10-30#$
+  $Rev: 73 $
+  $Author: yosiokat $
+ *---------------------------------------------------------------------------*/
+
+#ifndef	__RELOCATE_CHECKER_H__
+#define	__RELOCATE_CHECKER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <twl.h>
+
+// define data----------------------------------------------------------
+
+void RelocateCheckerInit( void );
+void RelocateCheckerMain( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __RELOCATE_CHECKER_H__
