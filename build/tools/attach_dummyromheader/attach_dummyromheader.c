b2a85004ea033363bcd4d95930c66ee32f9542c5
(no author) <(no author)@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-03-11 02:41:09 +0000

    copyright表記追加
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@857 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/tools/attach_dummyromheader/attach_dummyromheader.c
+++ build/tools/attach_dummyromheader/attach_dummyromheader.c
@@ -1,3 +1,20 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlIPL - tools - attach_dummyromheader
+  File:     attach_dummyromheader.c
+
+  Copyright 2008 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date:: 
+  $Rev: 
+  $Author: 
+ *---------------------------------------------------------------------------*/
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
