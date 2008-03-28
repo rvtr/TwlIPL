dd30d4881499ef2758028ebd2858b0ee3674d6fa
yosiokat <yosiokat@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-03-28 05:11:55 +0000

    無線ファームのゲームコード変更に対応。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@995 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/systemMenu_RED/Launcher/ARM9/src/loadWlanFirm.c
+++ build/systemMenu_RED/Launcher/ARM9/src/loadWlanFirm.c
@@ -112,10 +112,10 @@ void InstallFirmCallback(void* arg)
 
 BOOL GetFirmwareFilepath(char *path)
 {
-	u8 title[4] = { 'W','L','F','W' };
+	u8 title[4] = { 'H','W','F','A' };
 
 #if( USE_LCFG_STRING == 0 )
-    char *title0 = "WLFW";
+    char *title0 = "HWFA";
 #endif
     u32 titleID_hi;
     u32 titleID_lo;
