b738b5ed6b5e1d03c3ffd5846913d15b9ecee195
yoshida_teruhisa <yoshida_teruhisa@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-04-01 09:14:24 +0000

    X4ボード用ランチャーチェックに使うアプリケーションの雛形
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1028 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/tests/RomTypeTest/ARM9/src/RomTypeTest.h
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlIPL
+  File:     RomTypeTest.h
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+
+#ifndef	__ROM_TYPE_TEST_H__
+#define	__ROM_TYPE_TEST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <twl.h>
+
+// define data----------------------------------------------------------
+
+void RomTypeTestInit( void );
+void RomTypeTestMain( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __ROM_TYPE_TEST_H__
