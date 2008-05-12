c7e8f8228be2092cd85bee71a31e295f22348e59
kamikawa <kamikawa@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-05-12 10:47:28 +0000

    Makeが通るように修正。 これまでNandInitializerRed の ARM7コードをほぼそのまま使用していましたが、製品技術部のリクエスト対応を実装するにつれ SystemUpdater には実装すべきでないコードが増えきました。そのためARM7コードを独自に持つ形に変更しました。ビルドスイッチも増えすぎてかえって分雑なため。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1316 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/systemMenu_tools/SystemUpdater/ARM9.TWL/src/kami_pxi.c
@@ -0,0 +1,343 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - NandInitializer
+  File:     kami_pxi.c
+
+  Copyright 2008 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+
+#include <twl.h>
+#include "kami_pxi.h"
+#include "fifo.h"
+
+/*---------------------------------------------------------------------------*
+    定数定義
+ *---------------------------------------------------------------------------*/
+// 詰めてコピーする
+#define KAMI_PACK_U16(d, s)                   \
+    ((d)[0] = (u8)((*((u16*)s) >>  0) & 0xFF),  \
+     (d)[1] = (u8)((*((u16*)s) >>  8) & 0xFF))
+
+#define KAMI_PACK_U32(d, s)                   \
+    ((d)[0] = (u8)((*((u32*)s) >>  0) & 0xFF),  \
+     (d)[1] = (u8)((*((u32*)s) >>  8) & 0xFF),  \
+     (d)[2] = (u8)((*((u32*)s) >> 16) & 0xFF),  \
+     (d)[3] = (u8)((*((u32*)s) >> 24) & 0xFF))
+
+
+/*---------------------------------------------------------------------------*
+    型定義
+ *---------------------------------------------------------------------------*/
+typedef struct KamiWork
+{
+    BOOL lock;
+
+    KamiCommand  command;
+    KAMIPxiResult result;
+    KAMICallback  callback;
+    void*   arg;
+
+    u32 total;
+    u32 current;
+    u8* data;
+}
+KamiWork;
+
+/*---------------------------------------------------------------------------*
+    静的変数定義
+ *---------------------------------------------------------------------------*/
+static BOOL kamiInitialized;
+static KamiWork kamiWork;
+
+/*---------------------------------------------------------------------------*
+    内部関数定義
+ *---------------------------------------------------------------------------*/
+static BOOL KamiSendPxiCommand(KamiCommand command, u8 size, u8 data);
+static void KamiSendPxiData(u8 *pData);
+static void KamiPxiCallback(PXIFifoTag tag, u32 data, BOOL err);
+static void KamiDone(KAMIResult result);
+static void KamiWaitBusy(void);
+
+
+void KamiPxiInit( void )
+{
+    kamiWork.lock = FALSE;
+
+    PXI_Init();
+    while ( !PXI_IsCallbackReady(PXI_FIFO_TAG_KAMITEST, PXI_PROC_ARM7 ))
+    {
+    }
+    PXI_SetFifoRecvCallback(PXI_FIFO_TAG_KAMITEST, KamiPxiCallback);
+    if ( 0 > PXI_SendWordByFifo(PXI_FIFO_TAG_KAMITEST, KAMITEST_PXI_START_BIT | (KAMI_TEST_COMMAND << KAMITEST_PXI_COMMAND_SHIFT), 0))
+    {
+        return;
+    }
+}
+
+/*---------------------------------------------------------------------------*
+  Name:         NANDアクセス関数
+
+  Description:  
+
+  Arguments:    None.
+
+  Returns:      
+ *---------------------------------------------------------------------------*/
+
+KAMIResult kamiNandIo(u32 block, void* buffer, u32 count, BOOL is_read)
+{
+    OSIntrMode enabled;
+    u8  data[12];
+	int i;
+
+	// ロック
+    enabled = OS_DisableInterrupts();
+    if (kamiWork.lock)
+    {
+        (void)OS_RestoreInterrupts(enabled);
+        return KAMI_RESULT_BUSY;
+    }
+    kamiWork.lock = TRUE;
+    (void)OS_RestoreInterrupts(enabled);
+
+    kamiWork.callback = NULL;
+    kamiWork.arg = 0;
+    kamiWork.data = 0;
+
+	// データ作成
+	KAMI_PACK_U32(&data[0], &block);
+	KAMI_PACK_U32(&data[4], &buffer);
+	KAMI_PACK_U32(&data[8], &count);
+
+    if (KamiSendPxiCommand(KAMI_NAND_IO, 12, (u8)is_read))
+    {
+	    for (i = 0; i < 12; i+=3) 
+		{
+	        KamiSendPxiData(&data[i]);
+		}
+	    KamiWaitBusy();
+	    return (KAMIResult)kamiWork.result;
+    }
+    return KAMI_RESULT_SEND_ERROR;
+}
+
+/*---------------------------------------------------------------------------*
+  Name:         Nvramアクセス関数
+
+  Description:  
+
+  Arguments:    None.
+
+  Returns:      
+ *---------------------------------------------------------------------------*/
+
+KAMIResult kamiNvramIo(u32 address, void* buffer, u32 size, BOOL is_read)
+{
+    OSIntrMode enabled;
+    u8  data[12];
+	int i;
+
+	// ロック
+    enabled = OS_DisableInterrupts();
+    if (kamiWork.lock)
+    {
+        (void)OS_RestoreInterrupts(enabled);
+        return KAMI_RESULT_BUSY;
+    }
+    kamiWork.lock = TRUE;
+    (void)OS_RestoreInterrupts(enabled);
+
+    kamiWork.callback = NULL;
+    kamiWork.arg = 0;
+    kamiWork.data = 0;
+
+	// データ作成
+	KAMI_PACK_U32(&data[0], &address);
+	KAMI_PACK_U32(&data[4], &buffer);
+	KAMI_PACK_U32(&data[8], &size);
+
+    if (KamiSendPxiCommand(KAMI_NVRAM_IO, 12, (u8)is_read))
+    {
+	    for (i = 0; i < 12; i+=3) 
+		{
+	        KamiSendPxiData(&data[i]);
+		}
+	    KamiWaitBusy();
+	    return (KAMIResult)kamiWork.result;
+    }
+    return KAMI_RESULT_SEND_ERROR;
+}
+
+/*---------------------------------------------------------------------------*
+  Name:         kamiClearNandErrorLog
+
+  Description:  NVRAMのNANDエラー情報をクリアします。
+
+  Arguments:    None.
+
+  Returns:      
+ *---------------------------------------------------------------------------*/
+
+KAMIResult kamiClearNandErrorLog( void )
+{
+    OSIntrMode enabled;
+
+	// ロック
+    enabled = OS_DisableInterrupts();
+    if (kamiWork.lock)
+    {
+        (void)OS_RestoreInterrupts(enabled);
+        return KAMI_RESULT_BUSY;
+    }
+    kamiWork.lock = TRUE;
+    (void)OS_RestoreInterrupts(enabled);
+
+    kamiWork.callback = NULL;
+    kamiWork.arg = 0;
+    kamiWork.data = 0;
+
+    if (KamiSendPxiCommand(KAMI_CLEAR_NAND_ERRORLOG, 0, (u8)0))
+    {
+	    KamiWaitBusy();
+	    return (KAMIResult)kamiWork.result;
+    }
+    return KAMI_RESULT_SEND_ERROR;
+}
+
+/*---------------------------------------------------------------------------*
+	PXI関連
+ *---------------------------------------------------------------------------*/
+
+static BOOL KamiSendPxiCommand(KamiCommand command, u8 size, u8 data)
+{
+    u32 pxiData = (u32)(KAMITEST_PXI_START_BIT |
+            ((command << KAMITEST_PXI_COMMAND_SHIFT) & KAMITEST_PXI_COMMAND_MASK) |
+            ((size << KAMITEST_PXI_DATA_NUMS_SHIFT) & KAMITEST_PXI_DATA_NUMS_MASK) |
+            ((data << KAMITEST_PXI_1ST_DATA_SHIFT) & KAMITEST_PXI_1ST_DATA_MASK));
+    if (0 > PXI_SendWordByFifo(PXI_FIFO_TAG_KAMITEST, pxiData, 0))
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+static void KamiSendPxiData(u8 *pData)
+{
+    u32 pxiData = (u32)((pData[0] << 16) | (pData[1] << 8) | pData[2]);
+    while (0 > PXI_SendWordByFifo(PXI_FIFO_TAG_KAMITEST, pxiData, 0))
+    {
+    }
+}
+
+static void KamiPxiCallback(PXIFifoTag tag, u32 data, BOOL err)
+{
+#pragma unused( tag )
+    if (err)
+    {
+        KamiDone(KAMI_RESULT_FATAL_ERROR);
+        return;
+    }
+    if (data & KAMITEST_PXI_START_BIT)   // 先頭データ
+    {
+        if (data & KAMITEST_PXI_RESULT_BIT)
+        {
+            kamiWork.total = (u8)((data & KAMITEST_PXI_DATA_NUMS_MASK) >> KAMITEST_PXI_DATA_NUMS_SHIFT);
+            kamiWork.current = 0;
+            kamiWork.command = (KamiCommand)((data & KAMITEST_PXI_COMMAND_MASK) >> KAMITEST_PXI_COMMAND_SHIFT);
+            kamiWork.result = (KAMIPxiResult)((data & KAMITEST_PXI_1ST_DATA_MASK) >> KAMITEST_PXI_1ST_DATA_SHIFT);
+        }
+        else    // 未知のデータ
+        {
+            KamiDone(KAMI_RESULT_FATAL_ERROR);
+            return;
+        }
+    }
+    else    // 後続データ
+    {
+        if (kamiWork.data == NULL)
+        {
+            KamiDone(KAMI_RESULT_FATAL_ERROR);
+            return;
+        }
+        if (kamiWork.current < kamiWork.total-1)
+        {
+            kamiWork.data[kamiWork.current++] = (u8)((data & 0xFF0000) >> 16);
+        }
+        if (kamiWork.current < kamiWork.total-1)
+        {
+            kamiWork.data[kamiWork.current++] = (u8)((data & 0x00FF00) >> 8);
+        }
+        if (kamiWork.current < kamiWork.total-1)
+        {
+            kamiWork.data[kamiWork.current++] = (u8)((data & 0x0000FF) >> 0);
+        }
+    }
+    if (kamiWork.current == kamiWork.total-1)
+    {
+        KAMIResult result;
+        switch (kamiWork.result)
+        {
+        case KAMI_PXI_RESULT_SUCCESS:     // alias KAMI_PXI_RESULT_SUCCESS_TRUE
+            result = KAMI_RESULT_SUCCESS; // alias KAMI_RESULT_SUCCESS_TRUE
+            break;
+        case KAMI_PXI_RESULT_SUCCESS_FALSE:
+            result = KAMI_RESULT_SUCCESS_FALSE;
+            break;
+        case KAMI_PXI_RESULT_INVALID_COMMAND:
+            result = KAMI_RESULT_INVALID_COMMAND;
+            break;
+        case KAMI_PXI_RESULT_INVALID_PARAMETER:
+            result = KAMI_RESULT_INVALID_PARAMETER;
+            break;
+        case KAMI_PXI_RESULT_ILLEGAL_STATUS:
+            result = KAMI_RESULT_ILLEGAL_STATUS;
+            break;
+        case KAMI_PXI_RESULT_BUSY:
+            result = KAMI_RESULT_BUSY;
+            break;
+        default:
+            result = KAMI_RESULT_FATAL_ERROR;
+        }
+        KamiDone(result);
+        return;
+    }
+}
+
+extern void PXIi_HandlerRecvFifoNotEmpty(void);
+static void KamiWaitBusy(void)
+{
+    volatile BOOL *p = &kamiWork.lock;
+
+    while (*p)
+    {
+        if (OS_GetCpsrIrq() == OS_INTRMODE_IRQ_DISABLE)
+        {
+            PXIi_HandlerRecvFifoNotEmpty();
+        }
+    }
+}
+
+static void KamiDone(KAMIResult result)
+{
+    KAMICallback callback = kamiWork.callback;
+    void* arg = kamiWork.arg;
+    kamiWork.callback = NULL;
+    kamiWork.arg = NULL;
+    if (kamiWork.lock)
+    {
+        kamiWork.lock = FALSE;
+    }
+    if (callback)
+    {
+        callback(result, arg);
+    }
+}
---
+++ build/systemMenu_tools/SystemUpdater/ARM9.TWL/src/kami_write_nandfirm.c
@@ -0,0 +1,300 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - NandInitializer
+  File:     kami_write_nandfirm.c
+
+  Copyright 2008 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+
+#include <twl.h>
+#include <twl/fatfs.h>
+#include <nitro/card.h>
+#include <twl/nam.h>
+#include "kami_font.h"
+#include "kami_pxi.h"
+
+#include <firm/format/firm_common.h>
+#include "kami_write_nandfirm.h"
+
+/*---------------------------------------------------------------------------*
+    マクロ定義
+ *---------------------------------------------------------------------------*/
+
+// NANDファーム書き込みの際にNVRAMの未割り当て領域＋予約領域を０クリアする場合は定義します（開発用）
+//#define CLEAR_NON_ASIGNED_AREA_AND_RESERVED_AREA_ALL
+
+#define ROUND_UP(value, alignment) \
+    (((u32)(value) + (alignment-1)) & ~(alignment-1))
+
+/*---------------------------------------------------------------------------*
+    定数定義
+ *---------------------------------------------------------------------------*/
+
+#define NAND_BLOCK_BYTE 			0x200
+#define NAND_FIRM_START_OFFSET    	0x200
+
+#define NVRAM_PAGE_SIZE 0x100
+#define NVRAM_NORFIRM_RESERVED_ADDRESS     0x200
+#define NVRAM_NORFIRM_NANDBOOT_FLAG_OFFSET 0xff
+#define NVRAM_NORFIRM_NANDBOOT_FLAG        0x80
+
+#define NVRAM_NON_ASIGNED_AREA_ADDRESS     0x300
+
+/*---------------------------------------------------------------------------*
+    内部変数定義
+ *---------------------------------------------------------------------------*/
+
+static u8 sNvramPageSizeBuffer[NVRAM_PAGE_SIZE] ATTRIBUTE_ALIGN(32);	// ARM7からアクセスするためスタックでは駄目
+static u32 sReservedAreaEndAddress;
+
+/*---------------------------------------------------------------------------*
+  Name:         kamiWriteNandfirm
+
+  Description:  
+
+  Arguments:    no
+
+  Returns:      None.
+ *---------------------------------------------------------------------------*/
+BOOL kamiWriteNandfirm(const char* pFullPath, NAMAlloc allocFunc, NAMFree freeFunc)
+{
+    FSFile  file;
+
+    BOOL    open_is_ok;
+	BOOL    read_is_ok;
+	u8* pTempBuf;
+	u32 file_size;
+	u32 alloc_size;
+	u32 write_size;
+	BOOL result = TRUE;
+	u16 crc_w1, crc_w2;
+	u16 crc_r1, crc_r2;
+	u16 crc_norfirm_reserved_area_w, crc_norfirm_reserved_area_r;
+#ifdef CLEAR_NON_ASIGNED_AREA_AND_RESERVED_AREA_ALL
+	u32 write_offset;
+#endif
+
+	// .nandファイルオープン
+    FS_InitFile(&file);
+    open_is_ok = FS_OpenFile(&file, pFullPath);
+	if (!open_is_ok)
+	{
+		OS_Warning("Failure! FS_OpenFile");
+		return FALSE;
+	}
+
+	// サイズチェック
+	file_size  = FS_GetFileLength(&file) ;
+	if (file_size > (800*1024))
+	{
+		kamiFontPrintfConsoleEx(1, "too big file size!\n");
+		FS_CloseFile(&file);
+		return FALSE;
+	}
+
+	// バッファ確保
+	alloc_size = ROUND_UP(file_size, 32) ;
+	pTempBuf = allocFunc( alloc_size );
+	if (pTempBuf == NULL)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail Alloc()!\n");
+		FS_CloseFile(&file);
+		return FALSE;		
+	}
+
+	// .nandファイルリード
+	DC_InvalidateRange(pTempBuf, alloc_size);
+	read_is_ok = FS_ReadFile( &file, pTempBuf, (s32)file_size );
+	DC_StoreRange(pTempBuf, file_size);
+	if (!read_is_ok)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail FS_ReadFile!\n");
+		FS_CloseFile(&file);
+		freeFunc(pTempBuf);
+		return FALSE;
+	}
+
+	// ファイルクローズ
+	FS_CloseFile(&file);
+
+	// 書き込み前のCRCを計算
+	crc_w1 = SVC_GetCRC16( 0xffff, pTempBuf, sizeof(NORHeaderDS) );
+	crc_w2 = SVC_GetCRC16( 0xffff, pTempBuf+512, file_size-512 );
+
+	// まずNORHeaderDS領域を書き込む（40byte?）
+	if (kamiNvramWrite(0, (void*)pTempBuf, sizeof(NORHeaderDS)) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramWrite()\n");
+		result = FALSE;
+	}
+
+	// CRCを計算するので念のためにクリアしてからリードする
+	MI_CpuFill8( pTempBuf, 0xee, sizeof(NORHeaderDS) );
+	DC_FlushRange(pTempBuf, sizeof(NORHeaderDS));
+
+	// CRCチェックのためNvramからリード
+	if (kamiNvramRead(0, pTempBuf, sizeof(NORHeaderDS) ) == KAMI_RESULT_SEND_ERROR)
+	{
+	    kamiFontPrintfConsoleEx(1, "Fail kamiNvramRead()!\n");
+	}
+	DC_StoreRange(pTempBuf, sizeof(NORHeaderDS));
+
+	// 書き込み後のCRCを計算
+	crc_r1 = SVC_GetCRC16( 0xffff, pTempBuf, sizeof(NORHeaderDS) );
+
+	// NVRAM先頭部分のCRC比較
+	if ( crc_w1 != crc_r1 )
+	{
+		freeFunc(pTempBuf);
+		kamiFontPrintfConsoleEx(1, "Fail! CRC check %x!=%x\n", crc_w1, crc_r1);
+		return FALSE;
+	}
+
+	// nandfirm 起動フラグを立てる
+	MI_CpuClear8( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+	sNvramPageSizeBuffer[NVRAM_NORFIRM_NANDBOOT_FLAG_OFFSET] = NVRAM_NORFIRM_NANDBOOT_FLAG;
+	DC_FlushRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE);
+
+	// NORファームリザーブ領域の書き込みデータのCRCを計算
+	crc_norfirm_reserved_area_w = SVC_GetCRC16( 0xffff, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+
+	if (kamiNvramWrite(NVRAM_NORFIRM_RESERVED_ADDRESS, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramWrite()\n");
+		result = FALSE;
+	}
+
+	// CRCを計算するので念のためにクリアしてからリードする
+	MI_CpuFill8( sNvramPageSizeBuffer, 0xee, NVRAM_PAGE_SIZE );
+
+	// 読み込みはARM7が直接メモリに書き出すため
+	DC_FlushRange(sNvramPageSizeBuffer, NVRAM_PAGE_SIZE);
+
+	if (kamiNvramRead(NVRAM_NORFIRM_RESERVED_ADDRESS, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramRead()\n");
+		result = FALSE;
+	}
+
+	// 書き込み後のCRCを計算
+	DC_StoreRange(sNvramPageSizeBuffer, NVRAM_PAGE_SIZE);
+	crc_norfirm_reserved_area_r = SVC_GetCRC16( 0xffff, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+
+	// NORファームリザーブ領域のCRC比較
+	if ( crc_norfirm_reserved_area_w != crc_norfirm_reserved_area_r )
+	{
+		kamiFontPrintfConsoleEx(1, "Fail! Norfirm Reserved Area CRC check %x!=%x\n", crc_norfirm_reserved_area_w, crc_norfirm_reserved_area_r);
+		result = FALSE;
+	}
+
+#ifdef CLEAR_NON_ASIGNED_AREA_AND_RESERVED_AREA_ALL
+	DC_InvalidateRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+	// 未割り当て領域＋予約領域を０クリアします（開発用）
+	if (kamiNvramRead(NVRAM_CONFIG_DATA_OFFSET_ADDRESS, &sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramRead()\n");
+		result = FALSE;
+	}
+    sReservedAreaEndAddress = (u32)(*(u16 *)sNvramPageSizeBuffer << NVRAM_CONFIG_DATA_OFFSET_SHIFT) - 0xA00;// TWL WiFi設定 + NTR WiFi設定 を差し引く
+	//OS_Printf("end = %x\n", sReservedAreaEndAddress);
+
+	MI_CpuFill8( sNvramPageSizeBuffer, 0x00, NVRAM_PAGE_SIZE );
+	DC_FlushRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+
+	for (write_offset=NVRAM_NON_ASIGNED_AREA_ADDRESS; write_offset < sReservedAreaEndAddress; write_offset += NVRAM_PAGE_SIZE)
+	{
+		if (kamiNvramWrite(write_offset, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+		{
+			kamiFontPrintfConsoleEx(1, "Fail kamiNvramWrite()\n");
+			result = FALSE;
+		}
+	}
+	//OS_Printf("write_offset = %x\n", write_offset);
+#else
+	// 未割り当て領域先頭256byte＋予約領域を０クリアします
+
+	MI_CpuFill8( sNvramPageSizeBuffer, 0x00, NVRAM_PAGE_SIZE );
+	DC_FlushRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+
+	if (kamiNvramWrite(NVRAM_NON_ASIGNED_AREA_ADDRESS, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramWrite()\n");
+		result = FALSE;
+	}
+
+	DC_InvalidateRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+	if (kamiNvramRead(NVRAM_CONFIG_DATA_OFFSET_ADDRESS, &sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramRead()\n");
+		result = FALSE;
+	}
+    sReservedAreaEndAddress = (u32)(*(u16 *)sNvramPageSizeBuffer << NVRAM_CONFIG_DATA_OFFSET_SHIFT) - 0xA00;// TWL WiFi設定 + NTR WiFi設定 を差し引く
+
+	MI_CpuFill8( sNvramPageSizeBuffer, 0x00, NVRAM_PAGE_SIZE );
+	DC_FlushRange( sNvramPageSizeBuffer, NVRAM_PAGE_SIZE );
+
+	if (kamiNvramWrite(sReservedAreaEndAddress - 0x100, sNvramPageSizeBuffer, NVRAM_PAGE_SIZE) == KAMI_RESULT_SEND_ERROR)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiNvramWrite()\n");
+		result = FALSE;
+	}
+#endif
+
+	// NANDログ情報のクリア
+	if (kamiClearNandErrorLog() != KAMI_RESULT_SUCCESS)
+	{
+		kamiFontPrintfConsoleEx(1, "Fail kamiClearNandErrorLog()\n");
+		result = FALSE;		
+	}
+
+//	kamiFontPrintfConsoleEx(0, "NAND Firm Import Start!\n");
+
+	// NAND書き込み
+	write_size = file_size/NAND_BLOCK_BYTE + (file_size % NAND_BLOCK_BYTE != 0);
+	kamiNandWrite( NAND_FIRM_START_OFFSET/NAND_BLOCK_BYTE, pTempBuf+NAND_FIRM_START_OFFSET, write_size );	// ブロック単位、バイト単位、ブロック単位
+
+//	kamiFontPrintfConsoleEx(0, "Start CRC check\n");
+	kamiFontLoadScreenData();
+	
+	// CRCを計算するので念のためにクリアしてからリードする
+	MI_CpuClear8( pTempBuf, file_size );
+	DC_FlushRange(pTempBuf, file_size);
+
+	// CRCチェックのためNandからリード
+	if (kamiNandRead(0, pTempBuf, file_size/512 ) == KAMI_RESULT_SEND_ERROR)
+	{
+	    kamiFontPrintfConsoleEx(1, "kamiNandRead ... %s!\n", "ERROR");
+	}
+	DC_StoreRange(pTempBuf, file_size);
+
+	// 書き込み後のCRCを計算
+	crc_r2 = SVC_GetCRC16( 0xffff, pTempBuf+512, file_size-512 );
+
+	// NAND部分についてのCRCチェック
+	if (crc_w2 == crc_r2)
+	{
+//		kamiFontPrintfConsoleEx(0, "Success! CRC check %x==%x\n", crc_w2, crc_r2);
+	}
+	else
+	{
+		result = FALSE;
+		kamiFontPrintfConsoleEx(1, "Fail! CRC check %x!=%x\n", crc_w2, crc_r2);
+	}
+
+	// メモリ解放
+	freeFunc(pTempBuf);
+
+	return result;
+}
+
+
+
