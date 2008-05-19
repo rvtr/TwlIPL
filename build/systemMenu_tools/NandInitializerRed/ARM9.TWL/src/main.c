c6c3e143d10e670a46c14044283c490362a56909
yosiokat <yosiokat@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-05-19 01:18:27 +0000

    ボンディングオプション00での起動抑制をMARIOCLUB_VERSIONビルドスイッチが定義されている場合のみに制限。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1386 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/systemMenu_tools/NandInitializerRed/ARM9.TWL/src/main.c
+++ build/systemMenu_tools/NandInitializerRed/ARM9.TWL/src/main.c
@@ -87,12 +87,14 @@ TwlMain()
     (void)OS_EnableIrq();
     (void)GX_VBlankIntr(TRUE);
 
+#ifdef MARIOCLUB_VERSION
 	// ボンディングオプションが製品なら停止
 	if (SCFG_ReadBondingOption() == SCFG_OP_PRODUCT)
 	{
 		OS_Panic("Bonding Option == PRODUCT\n");
 	}
-
+#endif // MARIOCLUB_VERSION
+	
     // initialize file-system
 	FS_Init(FS_DMA_NOT_USE);
 	// SDカードの挿抜イベント監視コールバック設定
