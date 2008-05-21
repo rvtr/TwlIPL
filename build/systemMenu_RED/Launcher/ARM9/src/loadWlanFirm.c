47853b44028ae4d70300f175acebeeadda28305c
sato_masaki <sato_masaki@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-05-21 09:41:10 +0000

    ROM Header頭出し部分を完全に削除。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1443 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/systemMenu_RED/Launcher/ARM9/src/loadWlanFirm.c
+++ build/systemMenu_RED/Launcher/ARM9/src/loadWlanFirm.c
@@ -176,13 +176,6 @@ s32 ReadFirmwareHeader(char *path, u8 *buffer, s32 bufSize)
         OS_TWarning("FS_OpenFileEx(%s) failed.\n", path);
         return -1;
     }
-
-#if 0 // skip rom header seeking
-    if( FALSE == FS_SeekFile(file, sizeof(ROM_Header), FS_SEEK_SET) ) {
-        OS_TWarning("FS_SeekFile failed.\n");
-        return -1;
-    }
-#endif
     
     flen = FS_ReadFile(file, buffer, bufSize);
     if( flen == -1 ) {
@@ -207,12 +200,10 @@ s32 ReadFirmwareBinary(char *path, u32 offset, u8 *buffer, s32 bufSize)
         return -1;
     }
 
-#if 0 // skip rom header seeking
-    if( FALSE == FS_SeekFile(file, (s32)(sizeof(ROM_Header) + offset), FS_SEEK_SET) ) {
+    if( FALSE == FS_SeekFile(file, (s32)(offset), FS_SEEK_SET) ) {
         OS_TWarning("FS_SeekFile failed.\n");
         return -1;
     }
-#endif
     
     flen = FS_ReadFile(file, buffer, bufSize);
     if( flen == -1 ) {
