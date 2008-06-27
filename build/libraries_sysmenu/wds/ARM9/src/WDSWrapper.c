a18c6a7937f45f08cc5963b030b13e498c71ac91
yosiokat <yosiokat@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-06-27 00:17:14 +0000

    WDSのエラー修正。WDSWrapper追加。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/branches/20080626_SDK500fc_plus5_branch@1738 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/libraries_sysmenu/wds/ARM9/src/WDSWrapper.c
+++ build/libraries_sysmenu/wds/ARM9/src/WDSWrapper.c
@@ -1,3 +1,20 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlIPL
+  File:     WDS.c
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+
 //**********************************************************************
 /**
 //	@file		WDSWrapper.c
@@ -8,7 +25,7 @@
 //	@version	01.00
 //
 ***********************************************************************/
-#include "WDSWrapper.h"
+#include <sysmenu/WDSWrapper.h>
 #ifdef WDS_WITHDWC
 #include <dwc.h>
 #endif
