7ce80c0dfc8a05db6eea89a182dc457425043e37
yosiokat <yosiokat@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-07-01 00:56:36 +0000

    WDSTest, WDSWrapperTestがビルドできるよう修正。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1749 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/tests/WDSWrapperTest/src/main.c
+++ build/tests/WDSWrapperTest/src/main.c
@@ -1,4 +1,20 @@
-#include "WDSWrapper.h"
+/*---------------------------------------------------------------------------*
+  Project:  TwlIPL - tests - WDSWrapperTest
+  File:     main.c
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+#include <sysmenu/WDSWrapper.h>
 
 // VÉuÉâÉìÉNä÷êî
 static void VBlankIntr(void)
