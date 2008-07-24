594541beb127acf6e18611aa27ba8d92bd0325c9
yoshida_teruhisa <yoshida_teruhisa@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-07-24 01:58:04 +0000

    自己ハッシュチェックプログラム
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@1970 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/tests/HashCheckTemplate/crt/common/include/boot_sync.h
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - libraries - init - common
+  File:     boot_sync.h
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+#ifndef TWL_LIBRARIES_INIT_COMMON_BOOT_SYNC_H_
+#define TWL_LIBRARIES_INIT_COMMON_BOOT_SYNC_H_
+#ifdef  __cplusplus
+extern  "C" {
+#endif
+/*---------------------------------------------------------------------------*/
+
+#define	BOOT_SYNC_PHASE_1		1
+#define	BOOT_SYNC_PHASE_2		2
+#define	BOOT_SYNC_PHASE_3		3
+#define	BOOT_SYNC_PHASE_4		4
+#define	BOOT_SYNC_PHASE_5		5
+#define	BOOT_SYNC_PHASE_6		6
+
+/*---------------------------------------------------------------------------*/
+#ifdef __cplusplus
+}   /* extern "C" */
+#endif
+#endif  /* TWL_LIBRARIES_INIT_COMMON_BOOT_SYNC_H_ */
