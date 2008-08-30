2000ec0a0b4757a1e03da7d953afc956063eb253
(no author) <(no author)@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-08-30 04:18:55 +0000

    tocsu.sh, toaqua.rbでv0のものと、それ以外のものとで、出力ディレクトリが変わるように修正
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@2334 b08762b0-b915-fc4b-9d8c-17b2551a87ff

--- build/tests/CustomSystemUpdater/work/toaqua.rb
+++ build/tests/CustomSystemUpdater/work/toaqua.rb
@@ -25,6 +25,10 @@ REGION_DIR = {
     "A" => ["aus", "usa", "jp", "euro"],
 }
 
+VERUP_DIR = "verup"
+VERUP_CLS = "#{VERUP_DIR}/cls"
+VERUP_TAD = "#{VERUP_DIR}/tad"
+
 def proc(target_dir, output_dir)
     Find.find(target_dir){ |path|
         next unless File.file?(path) && (/[a-z0-9A-Z]{4}-.*v[0-9]+\.tad\.out/ =~ path) != nil
@@ -48,7 +52,14 @@ def proc(target_dir, output_dir)
         # リージョンを判別してコピー
         regions = REGION_DIR[File.split(src)[1][3..3]]
         for region in regions do
-            system("cp #{src} #{[output_dir, region, dest].join("/")}")
+            if version != "v0"
+                p version
+                p src
+                system("cp #{src} #{[output_dir, "#{VERUP_TAD}", dest].join("/")}")                
+                system("cp #{src.sub(/\.tad\.out/, ".cls")} #{[output_dir, "#{VERUP_CLS}", dest.sub(/\.tad/, ".cls")].join("/")}")
+            else
+                system("cp #{src} #{[output_dir, region, dest].join("/")}")
+            end
         end
     }
     
@@ -71,6 +82,9 @@ system("cp -rf #{src}/jp #{out}")
 #system("mkdir #{out}/usa")
 #system("mkdir #{out}/euro")
 #system("mkdir #{out}/jp")
+system("mkdir #{out}/#{VERUP_DIR}")
+system("mkdir #{out}/#{VERUP_CLS}")
+system("mkdir #{out}/#{VERUP_TAD}")
 
 proc(src, out)
 
--- build/tests/CustomSystemUpdater/work/tocsu.sh
+++ build/tests/CustomSystemUpdater/work/tocsu.sh
@@ -36,19 +36,22 @@ cp -rf ../$1/aus .
 cp -rf ../$1/usa .
 cp -rf ../$1/euro .
 
+mkdir verup
 mkdir $tmp
 
 find ../$1 -name "*.out" -exec cp {} $tmp \;
 
-find $tmp -regex "[^/]*/...[jaJA].*" | gawk -F/ '{printf("cp %s jp/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > jp.sh
-find $tmp -regex "[^/]*/...[uaUA].*" | gawk -F/ '{printf("cp %s aus/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > aus.sh
-find $tmp -regex "[^/]*/...[paPA].*" | gawk -F/ '{printf("cp %s euro/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > euro.sh
-find $tmp -regex "[^/]*/...[eaEA].*" | gawk -F/ '{printf("cp %s usa/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > usa.sh
+find $tmp -regex "[^/]*/...[jaJA].*v0\.tad\.out" | gawk -F/ '{printf("cp %s jp/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > jp.sh
+find $tmp -regex "[^/]*/...[uaUA].*v0\.tad\.out" | gawk -F/ '{printf("cp %s aus/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > aus.sh
+find $tmp -regex "[^/]*/...[paPA].*v0\.tad\.out" | gawk -F/ '{printf("cp %s euro/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > euro.sh
+find $tmp -regex "[^/]*/...[eaEA].*v0\.tad\.out" | gawk -F/ '{printf("cp %s usa/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > usa.sh
+find $tmp -regex "[^/]*/.*v[^0][0-9]*\.tad\.out" | gawk -F/ '{printf("cp %s verup/%s\n",$0, $2)}' | sed -e 's/\.out$//g' > verup.sh
 
 ./jp.sh
 ./aus.sh
 ./euro.sh
 ./usa.sh
+./verup.sh
 
 mv $tmp ../
 rm -rf *.sh
