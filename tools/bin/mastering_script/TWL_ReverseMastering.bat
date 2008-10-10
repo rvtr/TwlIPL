78725a76fc1c196cbb594baa4695b74675d30946
nishikawa_takeshi <nishikawa_takeshi@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2008-10-10 03:51:52 +0000

    マスタリングツール:ドラッグアンドドロップ用のバッチファイルを追加。
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@2609 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ tools/bin/mastering_script/TWL_ReverseMastering.bat
@@ -0,0 +1,66 @@
+@echo off
+
+rem
+rem mastering & comparing sample batch file
+rem
+rem    Simple usage: SRLファイルをドラッグすると%output_path%にマスタリング
+rem                  結果を出力し、正しくできたか検証を行う
+rem
+rem    要確認変数: mastering_tool, comparing_tool, output_path
+rem    補足: %~d0%~p0はバッチファイルのパスです
+rem    補足: %~d1%~p1は各入力ファイルのパスです
+rem
+
+rem 引数チェック
+set usage=USAGE: %~nx0 INPUT_FILE...
+if "%1" equ "" (
+	echo %usage%
+	goto end
+)
+
+rem パス設定
+set mastering_tool=%~d0%~p0mastering.TWL.exe
+set mastering_ini=%~d0%~p0mastering.TWL.ini
+
+rem ループ開始 (%0を使うのはここまで、%1を使うのはここから、%2以降は使わない)
+:begin
+
+rem 出力パス設定
+rem (ここでは入力が<path>\<file.srl>なら出力は<path>\m\mastered_<file.srl>という例)
+set output_path=%~d1%~p1
+set output_srl=%output_path%\%~n1.reverse%~x1
+set output_pro=%output_path%\%~n1.property
+
+rem その他の変数
+set input_srl=%1
+
+rem プログラム確認
+if not exist "%mastering_tool%" (
+	echo mastering tool "%mastering_tool%" is not found.
+	goto end
+)
+
+rem 入力ファイル確認
+if not exist %input_srl% (
+	echo %input_srl% is not found.
+	goto end
+)
+
+
+rem 逆マスタリング処理
+echo.
+echo %~nx1を逆マスタリングします。
+echo.
+%mastering_tool% %input_srl% %output_srl% -i %mastering_ini% -p %output_pro% -r
+
+if not %ERRORLEVEL% == 0 goto end
+
+rem 入力ファイルが残っているならbeginに戻る
+shift
+if "%1" neq "" goto begin
+
+rem 終了 (キー入力待ち)
+:end
+echo.
+echo すべての逆マスタリングが終了しました。
+pause
