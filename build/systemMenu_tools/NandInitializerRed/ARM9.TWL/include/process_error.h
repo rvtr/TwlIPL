4bf1817756cc07d401340ce0a2e4193d1b9ed0ce
kamikawa <kamikawa@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2009-05-27 01:46:01 +0000

    rev2809のコミットし忘れ分
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/trunk@2822 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/systemMenu_tools/NandInitializerRed/ARM9.TWL/include/process_error.h
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - NandInitializer
+  File:     process_import.h
+
+  Copyright 2008 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Date::            $
+  $Rev$
+  $Author$
+ *---------------------------------------------------------------------------*/
+
+#ifndef PROCESS_ERROR_H_
+#define PROCESS_ERROR_H_
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*===========================================================================*/
+
+#include <nitro.h>
+
+/*---------------------------------------------------------------------------*
+    å^íËã`
+ *---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*
+    ä÷êîíËã`
+ *---------------------------------------------------------------------------*/
+
+void* errorProcess(void);
+
+/*===========================================================================*/
+
+#ifdef	__cplusplus
+}          /* extern "C" */
+#endif
+
+#endif /* PROCESS_ERROR_H_ */
+
+/*---------------------------------------------------------------------------*
+  End of file
+ *---------------------------------------------------------------------------*/
