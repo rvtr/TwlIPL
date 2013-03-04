4a7de1415eee070d959478445533b03491e55507
mizu <mizu@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2013-03-04 07:39:50 +0000

    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/branches/20130304_launcher_save_app_to_sd_kai@3077 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/components/hyena.TWL/wram_regs/depend/ARM7-TS.LTD/Release/wram_regs.d
@@ -0,0 +1,3 @@
+obj/ARM7-TS.LTD/Release/wram_regs.o: wram_regs.c \
+	/cygdrive/d/Nitro_CwProjects/TWL_IPL_RED_cust/twlsdk/cache/include/twl.h.16M.ARM7.CODE_ARM.CW.CW_FORCE_EXPORT_SUPPORT.LINK_ISTD.RELEASE.TS.TS_VERSION-400.TWL.TWLLTD.C.mch \
+	/cygdrive/d/Nitro_CwProjects/TWL_IPL_RED_cust/twlsdk/include/twl/mi.h 
