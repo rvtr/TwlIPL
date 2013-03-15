a68ca158152093446a70842dc29f08d739a95856
mizu <mizu@b08762b0-b915-fc4b-9d8c-17b2551a87ff>
2013-03-15 01:52:16 +0000

    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/TwlIPL/branches/20130304_launcher_save_app_tosd_Remodel@3084 b08762b0-b915-fc4b-9d8c-17b2551a87ff

---
+++ build/libraries_sysmenu/hotsw/ARM7/include/dsCardCommon.h
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK
+  File:     
+ *---------------------------------------------------------------------------*/
+#ifndef __HOTSW_DSCARD_COMMON_H__
+#define __HOTSW_DSCARD_COMMON_H__
+
+#include 	<twl.h>
+#include 	<hotswTypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ===========================================================================
+// 	Function Describe
+// ===========================================================================
+// ■       ノーマルモードのコマンド       ■
+//	ノーマルモードのID読み込み
+HotSwState ReadIDNormal(CardBootData *cbd);
+
+//	ノーマルモードのBoot Segment(4Kbyte)読み込み
+HotSwState ReadBootSegNormal(CardBootData *cbd);
+
+//	ノーマルモードでステータスを読み込む
+HotSwState ReadStatusNormal(CardBootData *cbd);
+
+//	ノーマルモードでバッドブロックを置換
+HotSwState RefreshBadBlockNormal(CardBootData *cbd);
+
+//	ノーマルモードからセキュアモードへの変更
+HotSwState ChangeModeNormal(CardBootData *cbd);
+
+//	ノーマルモードからセキュア２モードへの変更
+HotSwState ChangeModeNormal2(CardBootData *cbd);
+
+// カード側のKeyTableをロード
+HotSwState LoadTable(void);
+
+// デバッガのROMエミュレーション情報読み込み
+HotSwState ReadRomEmulationInfo(SYSMRomEmuInfo *info);
+
+
+// ■       セキュアモードのコマンド       ■
+//	セキュアモードのID読み込み
+HotSwState ReadIDSecure(CardBootData *cbd);
+
+//	セキュアモードのSecure Segment(16Kbyte)読み込み
+HotSwState ReadSegSecure(CardBootData *cbd);
+
+//	セキュアモードのPNジェネレータON
+HotSwState SwitchONPNGSecure(CardBootData *cbd);
+
+//	セキュアモードのPNジェネレータOFF
+HotSwState SwitchOFFPNGSecure(CardBootData *cbd);
+
+//	セキュアモードのモード変更
+HotSwState ChangeModeSecure(CardBootData *cbd);
+
+
+// ■       ゲームモードのコマンド       ■
+//	ゲームモードのID読み込み
+HotSwState ReadIDGame(CardBootData *cbd);
+
+//  ゲームモードの指定ページ読み込み
+HotSwState ReadPageGame(CardBootData *cbd, u32 start_addr, void* buf, u32 size);
+
+//  ゲームモードでステータスを読み込む
+HotSwState ReadStatusGame(CardBootData *cbd);
+
+//	ゲームモードでバッドブロックを置換
+HotSwState RefreshBadBlockGame(CardBootData *cbd);
+
+
+// ■       内部関数  				     ■
+//	LEコマンドをBEへ変換してレジスタへセット
+void HOTSWi_SetCommand(GCDCmd64 *cndLE);
+
+//マジコン調査で追加
+void SendMgcComnd(u32 cmd_h,u32 cmd_l,u32 param);
+HotSwState ReadPageGame2(CardBootData *cbd, u32 start_addr, void* buf, u32 size,u32 param);
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif  // __HOTSW_DSCARD_COMMON_H__
